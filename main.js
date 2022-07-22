2f8cdbf43970163fc6933ce93a363722568fb31a
rahulk216 <rahulkakkadan216@gmail.com>
2022-07-22 20:05:16 +0530

    contact

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-    console.log('callback - particles.js config loaded');
+// particlesJS.load('particles-js', 'assets/particles.json', function() {
+//     console.log('callback - particles.js config loaded');
+// });
+
+document.addEventListener('DOMContentLoaded', function () {
+	if (document.getElementById('contact-slider') != null) {
+		var sliderNew = new Splide('#contact-slider', {
+			width: '100%',
+			arrows: false,
+		});
+		sliderNew.mount();
+		console.log('mounted');
+	}
 });
