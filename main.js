79f4bb623624e6f9a9899a98f4bcefc4c1471223
rahulk216 <rahulkakkadan216@gmail.com>
2022-07-26 12:37:31 +0530

    backdrop black for header

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
 const nav = document.querySelector('.nav');
 const open = document.querySelector('.open');
 const close = document.querySelector('.close');
-
+const body = document.querySelector('.body');
 const navItems = document.querySelector('.nav-items');
 const navLinks = document.getElementsByClassName('nav-links');
 console.log(navLinks);
 function navbar() {
 	open.addEventListener('click', () => {
 		navItems.classList.add('active-navbar');
+		body.classList.add('open-navbar');
 	});
 	close.addEventListener('click', () => {
 		navItems.classList.remove('active-navbar');
+		body.classList.remove('open-navbar');
 	});
 	for (var i = 0; i < navLinks.length; i++) {
 		navLinks[i].addEventListener('click', () => {
 			navItems.classList.remove('active-navbar');
+			body.classList.remove('open-navbar');
 		});
 	}
 }
